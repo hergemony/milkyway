d9f843ebd613eb7463343a69f81486b708912eef
Chi Doan <chi.doan86@gmail.com>
2022-07-26 08:35:40 +1000

    artist 2 and 3 addition

--- src/content.js
+++ src/content.js
@@ -2,20 +2,20 @@ const content = [
     {
       id: 1,
       title: 'ARTIST 1',
-      body: 'Anonymous. London based. Abstract paintings. Oil on Canvas. Urban focus.',
+      body: 'Anonymous. London based. Abstract paintings. Oil on Canvas.',
       image: 'duckonthegrass.jpg',
     },
     {
       id: 2,
       title: 'ARTIST 2',
-      body: "You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow.",
-      image: 'illustration-flowing-conversation.svg',
+      body: "Anonymous. Specialises in self-portraits. Graphite Pencil Artist. ",
+      image: 'artist2.png',
     },
     {
       id: 3,
       title: 'ARTIST 3',
-      body: "It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately.",
-      image: 'illustration-your-users.svg',
+      body: "Anonymous. Romanticist. ",
+      image: 'artist3.png',
     },
   ]
   
