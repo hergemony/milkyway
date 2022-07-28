e403bbf68af5d0326bd048d4a3835e204b9352b9
Chi Doan <chi.doan86@gmail.com>
2022-07-28 22:13:40 +1000

    further backend changes

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,25 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 
 //Routes
 //const striperoutes = require('./routes/stripe-routes');
 
 const app = express();
-
+const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //app.use('/api/stripe', striperoutes);
 
-app.listen(5000, () => {
-    console.log("Listening on port 5000....");
-
+app.use('/images', express.static(path.join(__dirname,'../frontend/public/images')));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+}
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+})
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}....`);
 });
